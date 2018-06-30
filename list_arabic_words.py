249e3688ba550cfc8501a98d9c3711deaf4c46d5
Hämäläinen, Mika K <mika.hamalainen@helsinki.fi>
2018-06-30 21:42:23 +0200

    senses OED

--- list_arabic_words.py
+++ list_arabic_words.py
@@ -9,7 +9,7 @@ def find_files(directory, pattern):
                 yield filename
 
 def extract_arabic_words(oed_path="/Users/mikahama/neologismi/oed"):
-	result =[]
+	result = {}
 	for file in find_files(oed_path+ "/dictionary.json/", "words.json"):
 		data = json.load(codecs.open(file, "r", encoding="utf-8"))
 		for item in data:
@@ -20,7 +20,7 @@ def extract_arabic_words(oed_path="/Users/mikahama/neologismi/oed"):
 					lang_lists = etymon["etymon_language"]
 					for lang_list in lang_lists:
 						if "Arabic" in lang_list:
-							result.append(lemma)
+							result[lemma] = item["sense_ids"]
 							break
 	json.dump(result, codecs.open("arabic_words_list.json", "w", encoding="utf-8"), indent=4)
 
