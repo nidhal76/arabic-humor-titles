7ddfee3a2c2a5b38ceb88251000d04c88019dd20
Hämäläinen, Mika K <mika.hamalainen@helsinki.fi>
2018-07-03 00:50:29 +0200

    first steps in getting more arabic words

--- list_arabic_words.py
+++ list_arabic_words.py
@@ -1,5 +1,7 @@
 #encoding: utf-8
-import codecs, json, os, fnmatch
+import codecs, json, os, fnmatch, glob
+import xml.etree.cElementTree as ET
+import urllib2, urllib
 
 def find_files(directory, pattern):
     for root, dirs, files in os.walk(directory):
@@ -24,4 +26,55 @@ def extract_arabic_words(oed_path="/Users/mikahama/neologismi/oed"):
 							break
 	json.dump(result, codecs.open("arabic_words_list.json", "w", encoding="utf-8"), indent=4)
 
-extract_arabic_words()
+def bnc_frequencies(bnc_path="/Users/mikahama/neologismi/bnc/Texts/"):
+	files = glob.glob(bnc_path+"*/*/*.xml")
+	freqs = {}
+	for file in files:
+		tree = ET.parse(file)
+		root = tree.getroot()
+		texts = root.findall("wtext")
+		for text in texts:
+			sentences = text.getiterator("s")
+			for sentence in sentences:
+				words = sentence.getiterator("w")
+				for word in words:
+					w = word.text
+					if w is not None:
+						if w not in freqs:
+							freqs[w] = 0
+						freqs[w] += 1
+	json.dump(freqs, codecs.open("data/bnc_frequencies.json", "w", encoding="utf-8"), indent=4)
+
+def find_wikipedia_pages():
+	words = json.load(codecs.open("arabic_words_list.json", "r", encoding="utf-8"))
+	wiki_results = codecs.open("data/wikipages.xml", "w", encoding="utf-8")
+	wiki_results.write("<root>")
+	for word in words:
+		url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+urllib.quote(word.encode('utf8'))+"&format=xml"
+		try:
+			response = urllib2.urlopen(url)
+			data = response.read()
+			wiki_results.write(data.decode('utf-8'))
+		except:
+			print word
+	wiki_results.write("</root>")
+	wiki_results.close()
+
+def list_wiki_urls():
+	text = codecs.open("data/wikipages.xml", "r", encoding="utf-8").read()
+	i = text.find("<Url")
+	while i != -1:
+		text = text[i:]
+		a = text.find(">")
+		text = text[a+1:]
+		a = text.find("<")
+		url = text[:a]
+		print url
+		i = text.find("<Url")
+
+
+#extract_arabic_words()
+#bnc_frequencies()
+#find_wikipedia_pages()
+list_wiki_urls()
+
