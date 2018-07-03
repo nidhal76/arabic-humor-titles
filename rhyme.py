ff5545a16c4e5d70dd0672255cbd58364f6d8281
Hämäläinen, Mika K <mika.hamalainen@helsinki.fi>
2018-07-03 18:43:31 +0200

    rhyme

--- rhyme.py
+++ rhyme.py
@@ -5,13 +5,16 @@ stops = ["a", "the", "and", "or", "of", "an"]
 esng = ESpeakNG()
 esng.voice = 'en-gb-x-rp'
 
-
-def allitieration(title):
+def __get_tokens(title):
 	tokens = title.lower().split(" ")
 	tokens_to_test = []  
 	for token in tokens:
 		if token not in stops:
 			tokens_to_test.append(token)
+	return tokens_to_test
+
+def allitieration(title):
+	tokens_to_test = __get_tokens(title)
 	start_sounds = []
 	for token in tokens_to_test:
 		pronOUnciation =  esng.g2p(token, ipa=2)
@@ -27,6 +30,43 @@ def allitieration(title):
 		else:
 			n_of_alliterating +=1
 	return n_of_alliterating / float(n_of_non_alliterating +n_of_alliterating)
-		
 
-#print allitieration("Beauty and the Beast OK OK")
+def __get_similar(word, words):
+	sims = []
+	for w in words:
+		if word == w:
+			sims.append(w)
+		elif len(word) > len(w) and word[-len(w):] == w:
+			sims.append(w)
+		elif len(word) < len(w) and w[-len(word):] == word:
+			sims.append(w)
+	return sims
+
+def rhyme(title):
+	consonants = "mnbvcxzlkjhgfdsptrwq"
+	tokens = __get_tokens(title)
+	unique_tokens = list(set(tokens))
+	pronunciations = []
+	for token in unique_tokens:
+		if token[0] == "y":
+			token = token[1:]
+		while True:
+			if token[0] in consonants:
+				token = token[1:]
+			else:
+				break
+
+		pronOUnciation =  esng.g2p(token, ipa=2)
+		pronunciations.append(pronOUnciation.replace(u"ˈ", ""))
+	n_of_rhyming = 0
+	n_of_non_rhyming = 0
+	for sound in pronunciations:
+		n = len(__get_similar(sound, pronunciations))
+		if n == 1:
+			n_of_non_rhyming += 1
+		else:
+			n_of_rhyming +=1
+	return n_of_rhyming / float(len(tokens))
+
+
+#print rhyme("No go hoe")
